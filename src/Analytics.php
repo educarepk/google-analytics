3a904fde56dafbd271e74f953040091538fe6f43
Shahzad Ahmad <shahzadahmad7786@gmail.com>
2023-01-09 15:51:36 +0500

    Create Analytics.php

---
+++ src/Analytics.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Educare\Analytics;
+
+use Educare\Analytics\Builder\AnalyticsBuilder;
+use Educare\Analytics\Analytics\Builder\Director;
+
+class Analytics implements \Educare\Analytics\Contracts\Analytics
+{
+    private $analytics = [];
+
+    private function buildData()
+    {
+        $director = new Director(new AnalyticsBuilder(), $this->analytics);
+        $analytics = $director->get();
+
+        return $analytics;
+    }
+
+    public function get(): array
+    {
+        return $this->buildData()->render;
+    }
+
+    public function getBuilder(): AnalyticsBuilder
+    {
+        return $this->buildData();
+    }
+
+    public function setDateRange(string $startDate, string $endDate)
+    {
+        $this->analytics[] = [
+            'method'     => 'setDateRange',
+            'parameters' => ['startDate' => $startDate, 'endDate' => $endDate],
+        ];
+    }
+
+    public function setMaxResults(int $maxResults)
+    {
+        $this->analytics[] = [
+            'method'     => 'setMaxResults',
+            'parameters' => $maxResults,
+        ];
+    }
+
+    public function setMetrics(array $metric)
+    {
+        $this->analytics[] = [
+            'method'     => 'setMetric',
+            'parameters' => $metric,
+        ];
+    }
+
+    public function setDimension(array $dimension)
+    {
+        $this->analytics[] = [
+            'method'     => 'setDimension',
+            'parameters' => $dimension,
+        ];
+    }
+
+    public function setDimensionFilter(string $dimensionName, string $operator, $expression)
+    {
+        $this->analytics[] = [
+            'method'     => 'setDimensionFilter',
+            'parameters' => ['dimensionName' => $dimensionName, 'operator' => $operator, 'expression' => $expression],
+        ];
+    }
+
+    public function setOrder(string $fieldName, string $orderType, string $sortType)
+    {
+        $this->analytics[] = [
+            'method'     => 'setOrder',
+            'parameters' => ['fieldName' => $fieldName, 'orderType' => $orderType, 'sortType' => $sortType],
+        ];
+    }
+
+    public function setAnalyticsReporting(array $params)
+    {
+        $this->analytics[] = [
+            'method'     => 'setAnalyticsReporting',
+            'parameters' => $params,
+        ];
+    }
+}
